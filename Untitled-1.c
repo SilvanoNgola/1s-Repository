41ed1b9de9f2fb30cea3f46e51c24abb99b39fac
Silvano Olímpio Gaspar Ngola <silvanongola2006@gmail.com>
2023-01-30 16:58:18 +0100

    Create Untitled-1.c

---
+++ Untitled-1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main (){
+
+setlocale(LC_ALL,"Portuguese");
+
+system ("color a0");
+printf ("Olha este é o meu primeiro projectinho no GITHUB então dou-lhes as boas-vindas!");
+
+return 0;
+}
